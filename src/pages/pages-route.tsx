8418b1c7863e01cd59c1b858081a5c1fbc59f7b2
ryomaejii <82492270+ryomaejii@users.noreply.github.com>
2024-05-12 22:18:37 +0900

    style: pages route title style

--- src/pages/pages-route.tsx
+++ src/pages/pages-route.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function PagesRoute() {
   return (
     <div className="py-20 px-8">
-      <h1>Pages Route</h1>
+      <h1 className="text-4xl font-bold">Pages Route</h1>
       <div className="mt-8">
         <Link href="/" className="underline text-2xl">
           Top
